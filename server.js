17dea73188e2e1d63116bc1872be919e45e5d59d
Konstanty Wrona <koswro@gmail.com>
2023-04-26 10:43:38 +0200

    Add db

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  {
+    id: 2,
+    author: 'Amanda Doe',
+    text: 'They really know how to make you happy.',
+  },
+];
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
