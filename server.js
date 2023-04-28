185cad56e1b11ef6d09ecd1efae43bfaae3d4f73
Konstanty Wrona <koswro@gmail.com>
2023-04-28 09:09:47 +0200

    Prepare for replit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const testimonials = require('./routes/testimonials.routes');
 const concerts = require('./routes/concerts.routes');
@@ -10,15 +11,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api/', testimonials);
 app.use('/api/', concerts);
 app.use('/api/', seats);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send('Not found...');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
