3221c4e20538b12aca2dc89ceb205caba709e1c6
Konstanty Wrona <koswro@gmail.com>
2023-04-28 06:50:35 +0200

    Change to db.testimonials

--- server.js
+++ server.js
@@ -3,32 +3,27 @@ const path = require('path');
 const cors = require('cors');
 const uuid = require('uuid').v4;
 
+const db = require('./db.js');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  {
-    id: 2,
-    author: 'Amanda Doe',
-    text: 'They really know how to make you happy.',
-  },
-];
-
 app.get('/testimonials', (req, res) => {
-  res.json(db);
+  res.json(db.testimonials);
 });
 
 app.get('/testimonials/:id', (req, res) => {
-  res.json(db.find((data) => data.id == req.params.id));
+  res.json(db.testimonials.find((data) => data.id == req.params.id));
 });
 
 app.get('/testimonials/random', (req, res) => {
   res.json(
-    db.find((req) => req.id === Math.floor(Math.random() * db.length) + 1)
+    db.testimonials.find(
+      (req) => req.id === Math.floor(Math.random() * db.length) + 1
+    )
   );
 });
 
@@ -36,7 +31,7 @@ app.post('/testimonials', (req, res) => {
   const { author, text } = req.body;
   const id = uuid();
   const newTestimonial = { id: id, author: author, text: text };
-  db.push(newTestimonial);
+  db.testimonials.push(newTestimonial);
   res.json({ message: 'ok!' });
 });
 
@@ -45,7 +40,9 @@ app.put(
   (req, res) => {
     const { author, text } = req.body;
     const id = +req.params.id;
-    const testimonial = db.find((testimonial) => testimonial.id === id);
+    const testimonial = db.testimonials.find(
+      (testimonial) => testimonial.id === id
+    );
     testimonial.author = author;
     testimonial.text = text;
     res.json({ message: 'ok!' });
@@ -59,8 +56,8 @@ app.delete(
   '/testimonials/:id',
   (req, res) => {
     const id = +req.params.id;
-    db.splice(
-      db.findIndex((testimonial) => testimonial.id === id),
+    db.testimonials.splice(
+      db.testimonials.findIndex((testimonial) => testimonial.id === id),
       1
     );
     res.json({ message: 'deleted' });
