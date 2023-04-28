6366095c8e796af384bd1bb1b4c0b9f43eca5bac
Konstanty Wrona <koswro@gmail.com>
2023-04-28 07:07:23 +0200

    Clean server.js and adapta for groupped endpoints

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
-const uuid = require('uuid').v4;
 
-const db = require('./db.js');
+const testimonials = require('./routes/testimonials.routes');
+const concerts = require('./routes/concerts.routes');
+const seats = require('./routes/seats.routes');
 
 const app = express();
 
@@ -11,61 +11,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.get('/testimonials', (req, res) => {
-  res.json(db.testimonials);
-});
-
-app.get('/testimonials/:id', (req, res) => {
-  res.json(db.testimonials.find((data) => data.id == req.params.id));
-});
-
-app.get('/testimonials/random', (req, res) => {
-  res.json(
-    db.testimonials.find(
-      (req) => req.id === Math.floor(Math.random() * db.length) + 1
-    )
-  );
-});
-
-app.post('/testimonials', (req, res) => {
-  const { author, text } = req.body;
-  const id = uuid();
-  const newTestimonial = { id: id, author: author, text: text };
-  db.testimonials.push(newTestimonial);
-  res.json({ message: 'ok!' });
-});
-
-app.put(
-  '/testimonials/:id',
-  (req, res) => {
-    const { author, text } = req.body;
-    const id = +req.params.id;
-    const testimonial = db.testimonials.find(
-      (testimonial) => testimonial.id === id
-    );
-    testimonial.author = author;
-    testimonial.text = text;
-    res.json({ message: 'ok!' });
-  },
-  (err) => {
-    console.log(err);
-  }
-);
-
-app.delete(
-  '/testimonials/:id',
-  (req, res) => {
-    const id = +req.params.id;
-    db.testimonials.splice(
-      db.testimonials.findIndex((testimonial) => testimonial.id === id),
-      1
-    );
-    res.json({ message: 'deleted' });
-  },
-  (err) => {
-    console.log(err);
-  }
-);
+app.use(testimonials);
+app.use(concerts);
+app.use(seats);
 
 app.use((req, res) => {
   res.status(404).send('Not found...');
