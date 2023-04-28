2110cc1512f34a2c7a18e2553a3710b4599643af
Konstanty Wrona <koswro@gmail.com>
2023-04-28 06:31:34 +0200

    Add endpoints

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const uuid = require('uuid').v4;
 
 const app = express();
 
@@ -15,6 +16,58 @@ const db = [
     text: 'They really know how to make you happy.',
   },
 ];
+
+app.get('/testimonials', (res, req) => {
+  res.json(db);
+});
+
+app.get('/testimonials/:id', (res, req) => {
+  res.json(db.find((data) => data.id == req.params.id));
+});
+
+app.get('/testimonials/random', (res, req) => {
+  res.json(
+    db.find((req) => req.id === Math.floor(Math.random() * db.length) + 1)
+  );
+});
+
+app.post('/testimonials', (res, req) => {
+  const { author, text } = req.body;
+  const id = uuid();
+  const newTestimonial = { id: id, author: author, text: text };
+  db.push(newTestimonial);
+  res.json({ message: 'ok!' });
+});
+
+app.put(
+  '/testimonials/:id',
+  (res, req) => {
+    const { author, text } = req.body;
+    const id = +req.params.id;
+    const testimonial = db.find((testimonial) => testimonial.id === id);
+    testimonial.author = author;
+    testimonial.text = text;
+    res.json({ message: 'ok!' });
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+app.delete(
+  '/testimonials/:id',
+  (res, req) => {
+    const id = +req.params.id;
+    db.splice(
+      db.findIndex((testimonial) => testimonial.id === id),
+      1
+    );
+    res.json({ message: 'deleted' });
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
