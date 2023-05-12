6eca655dbd628c98b918a220f3d8d67d47dd83e1
Konstanty Wrona <koswro@gmail.com>
2023-05-12 14:51:59 +0200

    Change routes

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -1,45 +1,14 @@
 const express = require('express');
-const uuid = require('uuid').v4;
-const db = require('../db');
+
+const ConcertControlller = require('../controllers/concerts.controller');
 
 const router = express.Router();
 
-router.route('/concerts').get((req, res) => {
-  res.json(db.concerts);
-});
-
-router.route('/concerts/:id').get((req, res) => {
-  res.json(db.concerts.find((data) => data.id == req.params.id));
-});
-
-router.route('/concerts').post((req, res) => {
-  const { author, text } = req.body;
-  const id = uuid();
-  const newConcerts = { id: id, author: author, text: text };
-  db.concerts.push(newConcerts);
-  res.json({ message: 'ok!' });
-});
-
-router.route('/concerts/:id').put((req, res) => {
-  const { author, text } = req.body;
-  const id = +req.params.id;
-  const concerts = db.concerts.find((data) => data.id === id);
-  const index = db.concerts.indexOf(concerts);
-  const changeConcerts = {
-    id: id,
-    author: author,
-    text: text,
-  };
-  db.concerts[index] = changeConcerts;
-  res.json({ message: 'ok!' });
-});
-
-router.route('/concerts/:id').delete((req, res) => {
-  const element = db.concerts.find((data) => data.id == req.params.id);
-  const index = db.concerts.indexOf(element);
-
-  db.concerts.splice(index, 1);
-  res.json({ message: 'ok' });
-});
+router.get('/concerts', ConcertControlller.getAllConcerts);
+router.get('/concerts/random', ConcertControlller.getRandomConcert);
+router.get('/concerts/:id', ConcertControlller.getConcertById);
+router.post('/concerts', ConcertControlller.createConcert);
+router.put('/concerts/:id', ConcertControlller.updateConcert);
+router.delete('/concerts/:id', ConcertControlller.deleteConcert);
 
 module.exports = router;
